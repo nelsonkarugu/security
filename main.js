cff925a4aa41c3ddced0135de6ac1d495a4b1e06
Nelson Karugu <nelsonmuthiga@gmail.com>
2021-01-19 21:33:17 -0800

    init

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ const Discord = require('discord.js');
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 const client = new Discord.Client();
-
 const prefix = '-';//if you want to send a command, your command must start with the - symbol.
-
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-
+//read commands folder
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -26,9 +24,8 @@ client.once('ready', () => {
 
 //when someone joins the server, they are automattically added to the role "member" and they recieve a welcome message 
 
-client.on('guildMemberAdd', guildMember =>{
+client.on('guildMemberAdd', guildMember => {
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'member');
- 
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('799550713067077652').send(`Welcome <@${guildMember.user.id}> to our Among Us server! Happy gaming!`)
 });
@@ -45,17 +42,21 @@ client.on('message', message => {
     if (command === 'ping') {
         client.commands.get('ping').execute(message, args);
     } else if (command == 'clear') {
-        client.commands.get('clear').execute(message, args)}
+        client.commands.get('clear').execute(message, args)
+    }
     else if (command == 'ban') {
         client.commands.get('ban').execute(message, args)
-    } else if (command == 'kick') {
-        client.commands.get('kick').execute(message, args)
-    } else if (command == 'mute') {
-        client.commands.get('mute').execute(message, args)
-    } else if (command == 'unmute') {
-        client.commands.get('unmute').execute(message, args)
     } 
-    
+    else if (command == 'kick') {
+        client.commands.get('kick').execute(message, args)
+    } 
+    else if (command == 'mute') {
+        client.commands.get('mute').execute(message, args)
+    } 
+    else if (command == 'unmute') {
+        client.commands.get('unmute').execute(message, args)
+    }
+
 });
 
 
